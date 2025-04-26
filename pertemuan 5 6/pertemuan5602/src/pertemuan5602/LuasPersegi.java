95f9bc7eae7c2a88256c94d06a82589c34532246
Elldajaleha20 <elldajaleha2003@gmail.com>
2025-04-26 14:36:12 +0800

    Add files via upload

---
+++ pertemuan 5 6/pertemuan5602/src/pertemuan5602/LuasPersegi.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuan5602;
+
+/**
+ *
+ * @Ellda 
+ * TGL 26-04-2025
+ */
+  // LuasPersegi.java
+public class LuasPersegi {
+    public static int hitungLuas(int sisi) {
+        return sisi * sisi;
+    }
+}
+
+    
+
---
+++ pertemuan 5 6/pertemuan5602/src/pertemuan5602/Pertemuan5602.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pertemuan5602;
+
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author Ellda meo
+ * TGL 26-04-2025
+ */
+// Pertemuan5602.java
+public class Pertemuan5602 {
+    public static void main(String[] args) {
+        int sisi = 8;
+        int luas = LuasPersegi.hitungLuas(sisi);
+        System.out.println("Luas persegi dengan sisi " + sisi + " adalah " + luas);
+    }
+}
